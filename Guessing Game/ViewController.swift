e86dd93240625e1de7dfbf96f20f7d9520ea3be7
Izakk Carrillo <izakkray@gmail.com>
2018-07-17 14:53:20 -0400

    Added in functionality to check our guess

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         randomNumber = Int(arc4random_uniform(101)) //Typecasting the UInt32 that arc4random_uniform() returns to an Int
-        gameStatusLabel.text = "\(randomNumber!)"
+        gameStatusLabel.text = "Good Luck!"
        
     }
     override func didReceiveMemoryWarning() {
@@ -30,7 +30,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func submitButtonTapped(_ sender: Any) {
-        gameStatusLabel.text = "You tapped the submit button"
+        //Creates a new constant by attempting to convert what the user has entered inti the answerField to an int. If it can't, we break out of the function.
+        guard let userGuess = Int(answerField.text!) else {
+            return
+        }
+        
+        //Check to make sure that the number that the user put in is between 0 and 100. Break out of the function if it isn't
+        if userGuess < 0 || userGuess > 100 {
+            return
+        }
+        
+        if userGuess == randomNumber {
+            gameStatusLabel.text = "Correct! You won!"
+        } else if userGuess < randomNumber {
+            gameStatusLabel.text = "Your guess was too low!"
+        } else {
+            gameStatusLabel.text = "Your guess was too high"
+        }
+        
     }
     
     @IBAction func resetButtonTapped(_ sender: Any) {
