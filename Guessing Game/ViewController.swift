d0b98c9a0c521a42c3157d51c203f43e4e91ee29
Izakk Carrillo <izakkray@gmail.com>
2018-07-17 11:55:51 -0400

    Created the UI for the application

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var gameStatusLabel: UILabel!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var answerField: UITextField!
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        gameStatusLabel.text = "We've changed the text!"
+        submitButton.setTitle("Submit", for: .normal)
+        resetButton.setTitle("Reset", for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
