cfb4f40f206fb3fe8aa626365fe93741e2d539e3
Izakk Carrillo <izakkray@gmail.com>
2018-07-17 13:41:41 -0400

    Added code to generate random number

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -11,27 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var gameStatusLabel: UILabel!
-    
     @IBOutlet weak var submitButton: UIButton!
-    
     @IBOutlet weak var resetButton: UIButton!
-    
     @IBOutlet weak var answerField: UITextField!
     
- 
+    var randomNumber: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        gameStatusLabel.text = "We've changed the text!"
-        submitButton.setTitle("Submit", for: .normal)
-        resetButton.setTitle("Reset", for: .normal)
+        randomNumber = Int(arc4random_uniform(101)) //Typecasting the UInt32 that arc4random_uniform() returns to an Int
+        gameStatusLabel.text = "\(randomNumber!)"
+       
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        gameStatusLabel.text = "You tapped the submit button"
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        gameStatusLabel.text = "You tapped the reset button"
+    }
+    
+    
 }
 
